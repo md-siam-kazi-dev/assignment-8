2bedd2db67585c4ecc6d7e256bb2cc825c14dfed
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-05-04 00:10:07 +0600

    all change url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
