013364a1808a2606cedd902f2bc6e3280930a6db
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-05-07 02:17:39 +0600

    add skeletoon

--- src/component/MainCourse.js
+++ src/component/MainCourse.js
@@ -5,7 +5,7 @@ import Skeleton from "./Skeleton";
 
 const MainCourse = () => {
     return (
-        <Suspense fallback={<Skeleton />}>
+        <Suspense fallback={<div className="container-div grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-10 mx-auto"><Skeleton /></div>}>
             <CourseContent />
         </Suspense>
     );
