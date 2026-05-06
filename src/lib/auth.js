f58450926503068081c45bbb6f319ad1f9c65eca
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-05-07 01:49:48 +0600

    bug fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -9,7 +9,7 @@ export const auth = betterAuth({
       emailAndPassword: { 
     enabled: true, 
   }, 
-   
+   baseURL: process.env.BETTER_AUTH_URL,
     socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID, 
