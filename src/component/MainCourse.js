b941c5bac786f68e43d6c2f392ecef864e37c85a
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-05-07 02:11:59 +0600

    all chekk redy for prod

--- src/component/MainCourse.js
+++ src/component/MainCourse.js
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import CourseContent from "./courseContent";
+import Skeleton from "./Skeleton";
 
 
 const MainCourse = () => {
     return (
-        <Suspense fallback={<div className="text-center mt-20">Loading Courses...</div>}>
+        <Suspense fallback={<Skeleton />}>
             <CourseContent />
         </Suspense>
     );
